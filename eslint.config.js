af8edf050f9a234d17f2616afd73d367665eba04
Brekk <brekk@brekkbockrath.com>
2021-02-07 08:23:38 +0100

    initial commit on standalone

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module"
+  },
+  parser: "@babel/eslint-parser",
+  rules: {
+    "prettier/prettier": [2, { semi: false }],
+    "no-extra-semi": 2,
+    "no-var": 2,
+    eqeqeq: [2, "smart"],
+    "arrow-parens": [2, "as-needed"],
+    "consistent-this": 0,
+    "func-names": [2],
+    "max-len": [1, 100, 2],
+    "new-cap": [2, { capIsNew: false }],
+    "no-trailing-spaces": [2, { skipBlankLines: true }],
+    "no-unused-vars": [2, { vars: "all", varsIgnorePattern: "^___" }],
+    "no-shadow": [2, { builtinGlobals: false, hoist: "all" }],
+    "import/named": 2,
+    "import/no-mutable-exports": 2,
+    "import/order": 2,
+    "import/extensions": [
+      2,
+      {
+        json: "always",
+        js: "never",
+      }
+    ],
+  },
+  extends: ["standard", "prettier"],
+  plugins: ["babel", "fp", "import", "prettier", "ramda"]
+}
